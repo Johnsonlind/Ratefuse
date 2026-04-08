a3bdadc50b44d81291e383c3e77aeae6a85981fc
Johnsonlind <152807479+Johnsonlind@users.noreply.github.com>
2026-04-08 08:06:29 +0800

    Delete src directory

--- src/modules/resources/MemberOnlyPlaceholder.tsx
+++
@@ -1,7 +0,0 @@
-export function MemberOnlyPlaceholder({ text }: { text: string }) {
-  return (
-    <div className="rounded-xl border border-dashed border-gray-300 bg-gray-100 p-3 text-sm text-gray-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-400">
-      {text}
-    </div>
-  );
-}
